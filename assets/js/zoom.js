d57542dff6e35cd5cab48a73a9efcff6a8c69216
eseyem <92292093+sh4nnja@users.noreply.github.com>
2025-07-18 06:29:28 +0800

    Delete assets directory

--- assets/js/zoom.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function() {
-    var scrollTop = window.scrollY;
-    var scale = 1 + scrollTop / 500;
-    document.getElementById("img-bg").style.transform = "scale(" + scale + ")";
-  });
